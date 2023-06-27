a5510a6056630f0d32b55b5294c6aff08be8e7f2
ndimah <ndimah22@protonmail.com>
2023-06-27 17:44:03 +0200

    Feat: improved the configuration with dotenv

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import "dotenv/config";
 import express from "express";
 
 
 const app = express();
+const {API_PORT: port} = process.env
 app.get("/", function (req, res) {
     res.send({message: "hello world from: " + req.url});
 });
-app.listen(3000);
+app.listen(port, function () {
+    console.log("server listening to port %s", port);
+});
